84ac5aef7494f8f2c813af8cda1a0b8e721d5a5b
Dominik Domagalski <dragenet@gmail.com>
2023-02-18 15:16:10 +0100

    Add UserCard component to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
-import { Button } from '@mui/material';
 import { SeoHead } from '~/components/SeoHead';
+import { UserCard } from '~/components/UserCard';
+import { Grid } from '@mui/material';
+import { mockedUsers } from '~/mocks/mockedUsers';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <SeoHead />
-      <Button variant="contained">Hello Next</Button>
+      <Grid container spacing={2} alignItems="stretch" sx={{ p: 3 }}>
+        {mockedUsers.slice(0, 8).map((user) => (
+          <Link
+            key={user.id}
+            href={{
+              pathname: '/user/[id]',
+              query: {
+                id: user.id,
+              },
+            }}
+            legacyBehavior
+          >
+            <Grid item xs={12} sm={6} md={4} sx={{ cursor: 'pointer' }}>
+              <UserCard user={user} />
+            </Grid>
+          </Link>
+        ))}
+      </Grid>
     </>
   );
 }
