d8d778f596ca1ad55abae951acaa7d32e48400b4
Dominik Domagalski <dragenet@gmail.com>
2023-02-18 15:15:53 +0100

    Add UserCard component

---
+++ src/components/UserCard/__tests__/UserCard.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import { UserCard } from '~/components/UserCard';
+import { mockedUser } from './mockedData';
+
+describe('UserCard', () => {
+  it('renders correctly with mocked user', () => {
+    const { container } = render(<UserCard user={mockedUser} />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/UserCard/constants.ts
@@ -0,0 +1,5 @@
+import { User } from '~/api';
+
+export interface UserCardProps {
+  user: User;
+}
