65553755aa6b77652ede020850baffce3854f053
Dominik Domagalski <dragenet@gmail.com>
2023-02-19 18:02:44 +0100

    Add AppBar component

---
+++ src/components/molecules/AppBar/AppBar.tsx
@@ -0,0 +1,32 @@
+import { AppBar as AppBarBase, Box, Grid, Typography } from '@mui/material';
+import { useContextState } from '~/store';
+import * as C from './constants';
+
+export const AppBar = ({ leftButton, rightButton }: C.AppBarProps) => {
+  const { currentUser } = useContextState();
+  return (
+    <Box marginBottom={6}>
+      <AppBarBase>
+        <Grid container direction="row" alignItems="center" minHeight="48px">
+          <Grid item xs={1}>
+            {leftButton}
+          </Grid>
+
+          <Grid item alignSelf="center" xs={10}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+              textAlign="center"
+            >
+              {currentUser?.name || process.env.NEXT_PUBLIC_APP_NAME}
+            </Typography>
+          </Grid>
+          <Grid item xs={1}>
+            {rightButton}
+          </Grid>
+        </Grid>
+      </AppBarBase>
+    </Box>
+  );
+};
